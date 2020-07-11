1dbac542681a54de7493e5a56b922706a8802a9a
Jared Miller <jared.fr.miller@gmail.com>
2020-07-11 16:06:58 -0400

    Turned SISO into a function
    Also wrote up the description in notese.
    
    Next step: MIMO

--- peak_impulse_siso.m
+++ peak_impulse_siso.m
@@ -1,4 +1,4 @@
-function [peak_val, xp_val, tp_val, Mp_1] = peak_impulse_siso(A, B, C, order, ranktol, Tmax)
+function [peak_val, out] = peak_impulse_siso(A, B, C, order, rank_tol, Tmax)
 %PEAK_IMPULSE_SISO  Find the maximum value of |Cx| for the impulse response
 %of a SISO linear system with dynamics x'=Ax+Bu, y=Cx.
 %Input:
@@ -14,21 +14,160 @@ function [peak_val, xp_val, tp_val, Mp_1] = peak_impulse_siso(A, B, C, order, ra
 %   tp_val:     Expectation of optimal time (or Inf if Tmax = Inf)
 %   Mp_1:       Top corner of moment matrix
 
-if nargin < 5
+if nargin < 6
     Tmax = Inf;    
 end
 
-if nargin < 4
-    ranktol = 1e-3;
+if nargin < 5
+    rank_tol = 1e-3;
 end
 
-if nargin < 3
+if nargin < 4
     order = 2;
 end
 
+%Parameters of relaxation
+n = size(A, 1);
 d = order*2;
+
+%Gloptipoly Options
+mset clear; warning('off','YALMIP:strict')
+mset('yalmip',true);
+mset(sdpsettings('solver', 'mosek'));
+
+
+%% Set up the measures
+mpol('x', n);  
+mpol('xp', n);  
+
+
+if Tmax == Inf
+    var = x;
+    varp = xp;
+else
+    mpol('t', 1);
+    mpol('tp', 1);
+    var = [t; x];
+    varp = [tp; xp];
+end
+
+%measures
+mu  = meas(var);   %occupation measure    
+mup = meas(varp); %peak measure
+
+v  = mmon(x, d);
+vp = mmon(xp, d);
+yp = mom(vp);
+
+%Liouville
+
+Ay = mom(diff(v, x)*A*x);
+
+if Tmax ~= Inf
+    Ay = mom(diff(v, t)) + Ay*Tmax;
+end
+
+%initial measure
+if Tmax == Inf
+    powers = genPowGlopti(n, d);
+    y0 = prod((B').^powers, 2);
+else
+    %starts at time 0
+    powers = genPowGlopti(n+1, d);
+    y0 = prod(([0; B]').^powers, 2);
+end
+
+%% Gloptipoly constraints
+Liou = Ay + (y0 - yp);
+
+mom_con = (Liou == 0);
+
+%support
+%maximum norm, approximate with magnification (max svd) of A.
+R = 10*svds(A, 1)*norm(B)^2;
+
+X  = (x'*x <= R^2);
+Xp = (xp'*xp <= R^2);
+
+if Tmax ~= Inf
+    X  = [X, t*(1-t) >= 0];
+    Xp = [Xp, tp*(1-tp) >= 0];
+end
+supp_con = [X, Xp];
+
+cost = (C*xp)^2;
+
+objective = max(cost);
+
+
+%% Solve the Problem
+cost = (C*xp)^2;
+
+objective = max(cost);
+
+P = msdp(objective, ...
+    mom_con, supp_con);
+
+
+
+%solve LMI moment problem    
+[status,obj,m,dual_rec]= msol(P);
+
+%Extract Moments (Primal)
+peak_val = sqrt(obj);
+
+Mp = double(mmat(mup));
+xp_out = double(mom(xp));
+
+if Tmax == Inf
+    Mp_1 = Mp(1:3, 1:3);
+    tp_out = Inf;
+else
+    Mp_1 = Mp(1:4, 1:4);
+    tp_out = double(mom(tp));
     
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+end
+    
+rankp = rank(Mp_1, rank_tol);
+
+%Extract Polynomials (Dual)
+
+syms xc [n, 1];
+
+if Tmax == Inf
+    vv = conj(monolistYalToGlop(xc, d));
+    p = dual_rec'*vv;
+    Lp = jacobian(p, xc)*A*xc;
+else
+    syms tc;
+    
+    vv = conj(monolistYalToGlop([T*tc, xc], d));
+    p = dual_rec'*vv;
+    Lp = diff(p, tc) + jacobian(p, xc)*A*xc;
+end
+
+
+pval = matlabFunction(p);
+Lpval = matlabFunction(Lp);
+
+%% package together outputs
+out = struct;
+out.peak_val = peak_val;
+out.obj = obj;
+out.xp = xp_out;
+out.tp = tp_out;
+out.Mp_1 = Mp_1;
+out.Mp = Mp;
+out.rankp = rankp;
+
+%level set: out.p + out.obj = 0
+
+%polynomials
+out.p = p;
+out.pval = pval;
+out.Lp = Lp;
+out.Lpval = Lpval;
+
+
 end
 
---
+++ siso_plot.m
@@ -0,0 +1,181 @@
+%% peak impulse response for a state space system
+%from the Chesi papers
+
+%starting to turn this into functions
+
+%right now deterministic system
+%then move to uncertainty
+
+
+
+SOLVE = 1;
+PLOT = 1;
+MD = 200; %mesh density for 3d
+Tmax = Inf;
+Tmax_plot = 20;
+
+if SOLVE
+    
+order = 2;
+ranktol = 0.05;
+
+
+nsys = 2;    
+if nsys == 1
+    %DC motor example
+    %constant Jm
+    bm = 0.2;
+    Kt = 1;
+    La = 0.5;
+    Ra = 1;
+    Ke = 0.5;
+    Jm = 1;
+    
+    A = [0, 1, 0; 0, -bm/Jm, Kt/Jm; 0, -Ke/La, -Ra/La];
+    B = [0; 0; 1/La];
+    C = [1, 0, 0];
+    
+    sys = ss(A, B, C, 0);
+    
+elseif nsys == 2
+    %random system
+    n = 6;
+    sys = rss(n);
+else
+    %second order system
+    %true upper bound is p* = 0.645
+%    sys = ss([0 1; -0.5 -1], [0; 1], [1 0], 0);
+    %sys = ss([0 1; -0.5 -1], [0; 1], [2 1], 0);
+end
+
+n = size(sys.A, 1);
+
+[peak_val, opt] =  peak_impulse_siso(sys.A, sys.B, sys.C, order, ranktol);
+
+end
+
+if PLOT
+    figure(1)
+clf
+xtraj = struct;
+[xtraj.y, xtraj.t, xtraj.x] = impulse(sys, Tmax_plot);
+margin = 0.2;
+xmin = min(xtraj.x) - 0.3;
+xmax = max(xtraj.x) + 0.3;
+trange = xtraj.t([1, end]);
+
+if (n==2) || (n==3)
+subplot(1,2,1)
+hold on
+if n == 3
+
+    plot3(xtraj.x(:, 1), xtraj.x(:, 2), xtraj.x(:, 3), 'DisplayName', 'state');
+    scatter3(sys.B(1), sys.B(2), sys.B(3), 100, 'ok', 'DisplayName', 'Initial Condition', 'LineWidth', 2)
+    %fi = fimplicit3(opt.p + opt.obj, [xmin(1), xmax(1), xmin(2), xmax(2),xmin(3), xmax(3)], 'MeshDensity',MD, ...
+    %        'EdgeColor', 'None','FaceColor', 'k', 'FaceAlpha', 0.3, ...
+    %        'DisplayName', 'Safety Contour');
+    if opt.rankp == 1
+        scatter3(opt.xp(1), opt.xp(2), opt.xp(3), 100, '*k', 'DisplayName', 'Peak Value', 'LineWidth', 2)
+    end
+    zlabel('x_3')
+else
+    %n = 2
+    plot(xtraj.x(:, 1), xtraj.x(:, 2), 'DisplayName', 'state', 'LineWidth', 3);
+    scatter(sys.B(1), sys.B(2), 100, 'ok', 'DisplayName', 'Initial Condition', 'LineWidth', 2)
+    
+    p_neg = line_range([sys.C, -peak_val], [xmin(1), xmax(1)], [xmin(2), xmax(2)]);
+    p_pos = line_range([sys.C, peak_val], [xmin(1), xmax(1)], [xmin(2), xmax(2)]);
+    
+    if opt.rankp == 1
+        scatter(opt.xp(1), opt.xp(2), 100, '*k', 'DisplayName', 'Peak Value', 'LineWidth', 2)
+    end
+    if ~isempty(p_neg)
+        plot([p_neg{1}(1), p_neg{2}(1)],  [p_neg{1}(2), p_neg{2}(2)], 'r--', 'Linewidth', 3, 'DisplayName', 'Peak Certification')
+    end
+    if ~isempty(p_pos)
+        plot([p_pos{1}(1), p_pos{2}(1)],  [p_pos{1}(2), p_pos{2}(2)], 'r--', 'Linewidth', 3, 'HandleVisibility','off')
+    end
+
+    
+    fi2 = fimplicit(opt.p  + opt.obj, [xmin(1), xmax(1), xmin(2), xmax(2)], ...
+            ':k', 'DisplayName', 'Peak Contour', 'LineWidth', 3, 'MeshDensity', 150);    
+end
+xlabel('x_1')
+ylabel('x_2')
+title(['Impulse Response State, order = ', num2str(order)])
+axis tight
+legend('location', 'northeast')
+    
+
+subplot(1,2,2)
+end
+hold on
+title(['Impulse Response Output, peak = ', num2str(peak_val, 3)])
+plot(xtraj.t, xtraj.y, 'Linewidth', 3, 'DisplayName', 'Impulse Response')
+plot(trange, peak_val*[1,1], 'r--', 'Linewidth', 3, 'DisplayName', 'Peak Certification')
+plot(trange, -peak_val*[1,1], 'r--', 'Linewidth', 3, 'HandleVisibility','off')
+plot(trange, [0, 0], ':k', 'HandleVisibility','off')
+legend('location', 'southeast')
+
+xlabel('t')
+ylabel('y')
+
+
+%% Nonnegative values
+figure(2)
+clf
+%get data along impulse response trajectory 
+xcell = num2cell(xtraj.x, 1);
+xtraj.v = opt.pval(xcell{:});
+xtraj.Lv = opt.Lpval(xcell{:});
+xtraj.cost = (xtraj.x * sys.C').^2;
+    
+
+   subplot(3,1,1)
+    
+    hold on        
+    plot(xtraj.t, xtraj.v + opt.obj, 'b', 'LineWidth', 2, 'DisplayName', 'Peak Traj.')           
+    
+    %plots
+    
+    plot(xlim, [0, 0], ':k',  'HandleVisibility','off')
+
+    hold off
+    title('Safety Function along Trajectories')
+    xlabel('time')
+    ylabel('v(x) - \gamma')
+    legend('location', 'northwest')
+    
+    subplot(3,1,2)
+    hold on
+    plot(xtraj.t, xtraj.Lv, 'b', 'LineWidth', 2, 'DisplayName', 'Peak Traj.')           
+    %plots
+    
+    plot(xlim, [0, 0], ':k',  'HandleVisibility','off')
+
+    hold off
+    title('Change in Safety Function along Trajectories')
+    xlabel('time')
+    ylabel('L_f v(x)')
+    legend('location', 'northwest')
+    
+    subplot(3,1,3)    
+    hold on
+    
+    p_comp = -xtraj.cost - xtraj.v;
+    plot(xtraj.t, p_comp, 'b', 'LineWidth', 2, 'DisplayName', 'Peak Traj.')           
+    
+    %plots
+    
+    plot(xlim, [0, 0], ':k',  'HandleVisibility','off')
+    hold off
+    
+    
+    title('Comparision with Objective Function')
+    xlabel('time')
+    ylabel('-cost(x) - v(x,t)')
+    legend('location', 'northwest')
+ 
+
+
+end
